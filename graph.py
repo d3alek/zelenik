1ff01164563c28fa1a6247fba35d58fde1e04a24
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-03-27 21:15:30 +0300

    Fix for the fix..

--- graph.py
+++ graph.py
@@ -69,7 +69,7 @@ def handle_graph(db, a_thing):
         # get keys in the latest senses state, this will result in senses omitted from graph
         # if latest update did not report them
         sense_types = sorted(senses[-1].keys()) 
-        if sense_types.get('time'):
+        if 'time' in sense_types:
             sense_types.remove('time')
     else:
         sense_types = []
