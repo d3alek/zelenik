c751b58aba8a3d40650f336580c94c7507c31304
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-04-27 16:56:17 +0300

    Optimizing Ledenik - down to 100kb from 150kb

---
+++ view/ledenik/scripts/main.js
@@ -0,0 +1,7 @@
+requirejs(["moment.min", "moment-timezone-with-data-20012-2022.min", "locale/bg", "switch"], function(util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+});
+
