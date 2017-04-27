16a22a8e62b29a1ef4158844d89a7b8819171167
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-04-27 16:23:55 +0300

    Adding ledenik-specific view files.
    Semi-live view and user-friendly switching work. Just simlink view/ledenik/index.html instead of view/index.html
    to make a device ledenik instead of zelenik.

---
+++ view/ledenik/switch.js
@@ -0,0 +1,96 @@
+// Reloading
+// Modified from: http://stackoverflow.com/a/11405793
+var reloading;
+
+function checkReloading() {
+    if (window.location.hash=="#noautoreload") {
+        document.getElementById("reloadCB").checked=false;
+    }
+    else {
+        reloading=setTimeout("window.location.reload();", 5000);
+        document.getElementById("reloadCB").checked=true;
+    }
+}
+
+function toggleAutoRefresh(cb) {
+    if (cb.checked) {
+        window.location.replace("#");
+        reloading=setTimeout("window.location.reload();", 5000);
+    } else {
+        window.location.replace("#noautoreload");
+        clearTimeout(reloading);
+    }
+}
+
+window.onload=checkReloading;
+
+// -Reloading
+
+// Configuration
+document.getElementById('configuration').style.display = 'none'
+
+function configure() {
+    configuration_shown = 
+    document.getElementById('configuration').style.display == 'block'
+    if (configuration_shown) {
+        document.getElementById('configuration').style.display = 'none'
+    }
+    else {
+        document.getElementById('configuration').style.display = 'block'
+    }
+}
+
+// -Configuration
+
+// Switches
+
+reported = JSON.parse(document.getElementById('reported').innerText)
+write = reported['state']['write']
+
+desiredInput = document.getElementById('desired-input')
+desired_mode = JSON.parse(desiredInput.textContent)['mode']
+
+for (output in write) {
+    checkbox = document.getElementById(output)
+    loading = document.getElementById(output+'-loading')
+    reported_checked = write[output]
+    desired_checked = desired_mode[output]
+    if (reported_checked == desired_checked) {
+        checkbox.checked = write[output]
+        checkbox.setAttribute("onclick", "postToServer("+output+")")
+        loading.style.display = 'none'
+    }
+    else {
+        checkbox.parentElement.style.display = 'none'
+    }
+}
+
+function postToServer(checkbox) {
+    checked = checkbox.checked == true ? 1 : 0
+    output = checkbox.id
+    console.log("Setting " + output + " to " + checked)
+    desiredInput = document.getElementById('desired-input')
+    desired = JSON.parse(desiredInput.textContent)
+    desired['mode'][output] = checked
+    desiredInput.textContent = JSON.stringify(desired, null, 4)
+
+    desiredForm = document.getElementById('desired-form')
+    desiredForm.submit()
+}
+
+// -Switches
+
+// Stale data warning
+moment.locale('bg')
+reported_utc = moment.utc(reported['timestamp_utc'])
+seconds_since_update = (moment.utc() - reported_utc) / 1000
+
+if (seconds_since_update > 60) {
+    connection_problem = document.getElementById('connection-problem')
+    connection_problem_text = document.getElementById('connection-problem-text')
+    connection_problem_text.textContent = connection_problem_text.textContent + " от " + reported_utc.tz('Europe/Sofia').format('lll')
+    connection_problem.style.display = 'block'
+
+}
+
+// --Stale data warning
