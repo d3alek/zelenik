74c8fff947159a0d58f09aefb2bd953c4b2a70e7
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-08-01 16:29:45 +0300

    Now expecting otselo_id_rsa in secret, update_db_from_live uses it instead of hardcoded username

--- dev/update_db_from_live.sh
+++ dev/update_db_from_live.sh
@@ -1,1 +1,1 @@
-sudo rsync -avz --delete --progress --rsh='ssh -p8902 -i /home/alek/.ssh/id_rsa' shiptechnic@zelenik.duckdns.org:/www/zelenik/db /www/zelenik
+sudo rsync -avz --delete --progress --rsh='ssh -p8902 -i /www/zelenik/secret/otselo_id_rsa' otselo@zelenik.duckdns.org:/www/zelenik/db /www/zelenik
