66b5e23fe086fe2b3e931ace6e77f174525bd250
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2017-10-22 11:11:55 +0300

    Premature update_website script

---
+++ update_website.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://git-scm.com/book/en/v1/Git-Tools-Subtree-Merging
+
+git checkout blog &&\
+git pull &&\
+git checkout landing &&\
+git pull &&\
+git checkout master &&\
+git merge --squash -s subtree --no-commit blog landing
+
